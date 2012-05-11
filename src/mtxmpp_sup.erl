6bd60f37aa2da052cfecc7f307d06debc5d0b2a6
Maxim Treskin <zerthurd@gmail.com>
2012-05-11 10:05:48 +0700

    Add copyright headers

--- src/mtxmpp_sup.erl
+++ src/mtxmpp_sup.erl
@@ -1,11 +1,6 @@
-%%%-------------------------------------------------------------------
-%%% @author Maxim Treskin <mtreskin@metachord.com>
-%%% @copyright (C) 2011, Maxim Treskin
-%%% @doc
-%%%
-%%% @end
-%%% Created : 18 Oct 2011 by Maxim Treskin <mtreskin@metachord.com>
-%%%-------------------------------------------------------------------
+%%% @copyright  2012 Metachord Ltd.
+%%% @author     Max Treskin <mtreskin@metachord.com>
+
 -module(mtxmpp_sup).
 
 -include_lib("metalkia_core/include/mt_log.hrl").
